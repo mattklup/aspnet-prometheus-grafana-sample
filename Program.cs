f9a479445e7cca829da1b96f923bd65b15bc8962
mattklup <mattklup@microsoft.com>
2021-08-11 11:25:04 -0700

    Basic app

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+        {
+            return CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.Configure(applicationBuilder => {
+                        applicationBuilder.Use(async (context, next) => {
+                            await context.Response.WriteAsync($"hi, you wanted '{context.Request.Path}'");
+                        });
+                    });
+                });
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+```bash
+$ docker build -t aspnetapp .
+$ docker run -it --rm -p 8080:80 --name myapp aspnetapp
+```
