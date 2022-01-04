560bafb01235b799a259d437b48a8edcbf43d98e
mattklup <mattklup@microsoft.com>
2022-01-04 11:11:23 -0800

    Refactor dotnetcore code to src folder

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pushd ./web
+pushd ./src/web
 docker build -t mattklup/aspnetcore-5:local .
 popd
 
-pushd ./backend
+pushd ./src/backend
 docker build -t mattklup/aspnetcorebackend-5:local .
 popd
